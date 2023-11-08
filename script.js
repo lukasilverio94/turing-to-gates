c68cde95d5dbfd63bd4b6d222b9dae1888e43a0a
lukasilverio94 <lukasilverio94@gmail.com>
2023-11-09 00:38:42 +0100

    changed some margin/padding

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Selecting by toggle-img class.
-$(".toggle-img").click(function () {
-  const currentAttr = $(this).attr("src"); //Current Attribute
-  const altAttr = $(this).attr("alt-src"); //Alternative Attribute
+$("document").ready(function () {
+  $(".toggle-img").click(function () {
+    const currentAttr = $(this).attr("src"); //Current Attribute
+    const altAttr = $(this).attr("alt-src"); //Alternative Attribute
 
-  //toggle the "alternative src attribute"
-  $(this).attr("src", altAttr);
-  $(this).attr("alt-src", currentAttr);
+    //toggle the "alternative src attribute"
+    $(this).attr("src", altAttr);
+    $(this).attr("alt-src", currentAttr);
+  });
 });
